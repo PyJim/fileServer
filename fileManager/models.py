f017bd2f0194d6b69d1cc421e42f9ec7bb39ed79
Jimmy Essel <jimmykodwoessel@gmail.com>
2024-04-02 11:05:06 +0000

    file model working

--- fileManager/models.py
+++ fileManager/models.py
@@ -2,32 +2,32 @@ import os
 from django.db import models
 
 
-# create your models here
-
 class File(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255, blank=False)
     description = models.TextField(blank=True)
-    path = models.URLField(blank=False)
+    file = models.FileField(upload_to='uploads/', default='')
     date = models.DateTimeField(auto_now_add=True)
     file_type = models.CharField(max_length=50, blank=True)
     downloads_count = models.IntegerField(default=0)
     emailed_count = models.IntegerField(default=0)
 
     def save(self, *args, **kwargs):
+        # Set the path attribute to the URL of the uploaded file
+        self.path = self.file.url
+
         # Automatically determine the file type based on file extension
-        if self.path_or_url:
-            _, file_extension = os.path.splitext(self.path_or_url)
-            if file_extension:
-                file_extension = file_extension.lower()
-                if file_extension in ['.jpg', '.jpeg', '.png', '.gif', '.bmp']:
-                    self.file_type = 'image'
-                elif file_extension in ['.pdf', '.doc', '.docx', '.txt', '.ppt', '.xls', '.xlsx']:
-                    self.file_type = 'document'
-                elif file_extension in ['.mp4', '.avi', '.mkv', '.mov', '.wmv']:
-                    self.file_type = 'Video'
-                else:
-                    self.file_type = 'unknown'
+        _, file_extension = os.path.splitext(self.file.name)
+        if file_extension:
+            file_extension = file_extension.lower()
+            if file_extension in ['.jpg', '.jpeg', '.png', '.gif', '.bmp']:
+                self.file_type = 'image'
+            elif file_extension in ['.pdf', '.doc', '.docx', '.txt', '.ppt', '.xls', '.xlsx', '.csv']:
+                self.file_type = 'document'
+            elif file_extension in ['.mp4', '.avi', '.mkv', '.mov', '.wmv']:
+                self.file_type = 'video'
+            else:
+                self.file_type = 'unknown'
         super().save(*args, **kwargs)
 
     def __str__(self):
--- fileManager/views.py
+++ fileManager/views.py
@@ -14,37 +14,6 @@ def files_page(request):
     else:
         user = request.user
         email = user.email
-        # files = File.objects.all()
-        files = [
-            {
-                'id': 'id',
-                'title': 'How to Bake a Cake',
-                'description': 'Learn step-by-step instructions on baking a delicious cake.',
-                'path': 'cake.jpg',
-                'date': 'January 3, 2023',
-                'file_type': 'image',
-                'downloads_count': 5,
-                'emailed_count': 7,
-            },
-            {
-                'id': 'id',
-                'title': '10 tips for fitness',
-                'description': 'Discover essential tips for maintaining a healthy fitness routine.',
-                'path': 'fit.jpg',
-                'date': 'June 3, 2023',
-                'file_type': 'document',
-                'downloads_count': 3,
-                'emailed_count': 2,
-            },
-            {
-                'id': 'id',
-                'title': 'Introduction to AI',
-                'description': 'Explore the basics of Artificial Intelligence and its applications.',
-                'path': 'ai.jpg',
-                'date': 'January 3, 2023',
-                'file_type': 'video',
-                'downloads_count': 4,
-                'emailed_count': 1,
-            }
-        ]
+        files = File.objects.all()
+        
         return render(request, 'files.html', {'user': user, 'files': files})
