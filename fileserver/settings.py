10a9e686facdbc05ab87618eb114c7d993e7220a
Jimmy Essel <jimmykodwoessel@gmail.com>
2024-04-04 13:40:58 +0000

    pre deployment

--- fileserver/settings.py
+++ fileserver/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
