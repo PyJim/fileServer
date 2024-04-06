38e4e6c276d4d3c40bb3a77b37aee6b8daa847f5
Jimmy Essel <jimmykodwoessel@gmail.com>
2024-04-06 05:15:41 +0000

    file manager unit tests added

--- fileManager/tests.py
+++ fileManager/tests.py
@@ -1,21 +1,24 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client,TransactionTestCase
 from django.urls import reverse
 from .models import File
 from userAuth.models import User
-from userAuth.backends import EmailBackend  # Import the custom authentication backend
+from userAuth.backends import EmailBackend
+from common_test_utils import CommonTestUtils
 
 
-class TestViews(TestCase):
+class TestViews(TransactionTestCase):
 
     def setUp(self):
         self.client = Client()
         self.feed_url = reverse('feed')
-        self.file = File.objects.create(title='Test File', description='Test Description', file='car.jpg')
+        self.file = File.objects.create(title='Test File', description='Test Description', file='test.txt')
         self.user = User.objects.create_user(email='test@example.com', password='password')
 
         # Authenticate user using EmailBackend and force login
         self.authenticated_user = EmailBackend().authenticate(username='test@example.com', password='password')
         self.client.force_login(self.authenticated_user)
+
+        
 
     def test_files_page_GET_authenticated(self):
         response = self.client.get(self.feed_url)
@@ -51,18 +54,24 @@ class TestViews(TestCase):
         self.assertRedirects(response, reverse('login') + '?next=' + reverse('email_file', args=[self.file.id]))
 
     def test_email_file_POST_authenticated(self):
+        test_file = CommonTestUtils.create_test_file()
         response = self.client.post(reverse('email_file', args=[self.file.id]), {'email': 'test@example.com'})
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse('feed'))
+        CommonTestUtils.cleanup_uploaded_files()
 
     def test_email_file_POST_unauthenticated(self):
         self.client.logout()
+        test_file = CommonTestUtils.create_test_file()
         response = self.client.post(reverse('email_file', args=[self.file.id]), {'email': 'test@example.com'})
         # Redirect to login page when not authenticated
         self.assertRedirects(response, reverse('login') + '?next=' + reverse('email_file', args=[self.file.id]))
+        CommonTestUtils.cleanup_uploaded_files()
 
-    def test_download_file(self):
+    def test_download_file_authenticated(self):
+        test_file = CommonTestUtils.create_test_file()
         response = self.client.get(reverse('download_file', args=[self.file.id]))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['Content-Type'], 'application/force-download')
-        self.assertEqual(response['Content-Disposition'], f'attachment; filename=car.jpg')
+        self.assertEqual(response['Content-Disposition'], f'attachment; filename=test.txt')
+        CommonTestUtils.cleanup_uploaded_files()
